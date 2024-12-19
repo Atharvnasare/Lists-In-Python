31d8644c18496e3f16b892e3ceea63b1550db413
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-19 23:45:57 +0530

    Update List_method.py

--- List_method.py
+++ List_method.py
@@ -1,7 +1,9 @@
-list = [ "c" , "a" , "b" , "e" , "d"]
+list = [ 2,3,6,1,4,5]
 
-list.append("f")
+list.append(7)
 list.sort()
 list.sort(reverse=True)
 list.reverse()
+list.remove(3)
+list.pop(5)
 print(list)
